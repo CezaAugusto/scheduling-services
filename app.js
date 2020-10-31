ae7738bac73af6988519d9855b853c97f5bba9a7
Cezar Augusto Barbosa de Souza <49259391+CezaAugusto@users.noreply.github.com>
2020-10-31 18:48:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Aplication listening at http://localhost:${port}`)
+    console.log(`Application listening at http://localhost:${port}`)
 })
